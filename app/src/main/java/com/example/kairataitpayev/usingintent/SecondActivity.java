2f15cecc0a9ccd90661c39a9ed06b67dfcce060a
Kairat <kairat.other@gmail.com>
2014-09-11 10:54:23 +0600

    Initializing

---
+++ app/src/main/java/com/example/kairataitpayev/usingintent/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.kairataitpayev.usingintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kairat.aitpayev on 9/11/2014.
+ */
+public class SecondActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
